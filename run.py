707280a041fb441051a2f761157b581743cdf88a
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2017-11-22 18:02:01 +0100

    Implement duration measuring

--- run.py
+++ run.py
@@ -1,23 +1,38 @@
 from src.main.python.repository import Repository
 from src.main.python.configuration import Configuration
 from datetime import date
+import time
 
 
 def pull_questions():
     repository = Repository()
 
     periods = [
+        year_period(2014),
         year_period(2015),
-        year_period(2016),
-        year_period(2017)
+        year_period(2016)
     ]
 
+    total_start_time = time.time()
+
     for start, end in periods:
+        period_start_time = time.time()
+
         repository.questions.load_period(
-            {'from': start, 'to': end, 'depth': 5}
+            {'from': start, 'to': end, 'depth': 3}
         )
         repository.resolve()
         repository.commit()
+
+        period_end_time = time.time()
+
+        print("Period downloading took: " + \
+            str(period_end_time - period_start_time))
+
+    total_end_time = time.time()
+
+    print("Complete downloading took: " + \
+        str(total_end_time - total_start_time))
 
 def year_period(year):
     return [date(year, 1, 1), date(year, 12, 31)]
